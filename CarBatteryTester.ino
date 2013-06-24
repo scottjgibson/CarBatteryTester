e2bce3dfb6cd95879622d31e6365b2e69e537c41
Scott Gibson <scottgibson@gmail.com>
2013-06-24 16:42:32 -0300

    Update CarBatteryTester.ino

--- CarBatteryTester.ino
+++ CarBatteryTester.ino
@@ -1,7 +1,13 @@
-
 //Pin A0 - Current reading (across 0.1 ohm resistor)
-//Pin A1 - Battery Voltage (use a 1/4 voltage divider)
+//Pin A1 - Battery Voltage 
 
+//My divider is a 10k and 4.7k resitor;  4.7k / (10k+4.7k)
+//The value hear was measured with the circuit hooked up for greater accuracy
+#define DIVIDER_RATIO 4.5
+
+//Current Sense Resistor value; I used a 0.1ohm resistor (what I had onhand)
+// Make sure you wont exceed the maximum input voltage on the arduino (5V)
+#define CS_RESISTOR_VALUE 0.1
 
 void setup() {
   Serial.begin(9600);
@@ -16,13 +22,13 @@ void setup() {
 
 
 void loop() {
-  //Current in amps is 10 times the read value because I am using a 
-  //0.1ohm resistor
-  int current = analogRead(A0)*10;
+  //I = V/R
+  //The map is needed because the ADC reads 0-1024 for the range 0-5V
+  int current = map(analogRead(A0), 0, 1024, 0, 5) / RESISTOR_VALUE;
   
   //Voltage is 4 times the read value due to the external resistor divider
   //measure your voltage divider and adjust as needed.
-  int voltage = analogRead(A1)*4;
+  int voltage = analogRead(A1)*DIVIDER_RATIO;
 
 
   Serial.print(current);
